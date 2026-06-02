e662fb0674d2c72a8b604bf5e00749aeae111018
YK-Digital <yuliankanters2011@gmail.com>
2026-06-02 16:54:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 
 const path = require('path')
+const fs = require('fs')
+
 const html = path.join(__dirname, '/html');
 app.use(express.static(html))
 
@@ -13,9 +15,48 @@ const maxHttpBufferSizeInMb = parseInt(process.env.MAX_HTTP_BUFFER_SIZE_MB || '1
 const io = require("socket.io")(http, {
   maxHttpBufferSize: maxHttpBufferSizeInMb * 1024 * 1024,
 });
+
 let messageCache = [];
-// default cache size to zero. override in environment
 let cache_size = process.env.CACHE_SIZE ?? 0
+
+// ====================
+// BLOCKED WORDS SYSTEM
+// ====================
+
+let badWords = [];
+
+function loadBadWords() {
+	try {
+		badWords = fs.readFileSync('/data/blocked-words.txt', 'utf8')
+			.split('\n')
+			.map(w => w.trim())
+			.filter(Boolean);
+
+		console.log(`Loaded ${badWords.length} blocked words.`);
+	} catch (err) {
+		console.error('Failed to load blocked words:', err.message);
+		badWords = [];
+	}
+}
+
+function filterMessage(text) {
+	if (!text) return text;
+
+	let out = text;
+
+	badWords.forEach(word => {
+		const escaped = word.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+		const regex = new RegExp(escaped, 'gi');
+		out = out.replace(regex, '*'.repeat(word.length));
+	});
+
+	return out;
+}
+
+loadBadWords();
+setInterval(loadBadWords, 30000);
+
+// ====================
 
 http.listen(port, function(){
 	console.log("Starting server on port %s", port);
@@ -23,55 +64,51 @@ http.listen(port, function(){
 
 const users = [];
 let msg_id = 1;
+
 io.sockets.on("connection", function(socket){
 	console.log("New connection!");
 
 	var nick = null;
 
 	socket.on("login", function(data){
-		// Security checks
+
 		data.nick = data.nick.trim();
 
-		// If is empty
 		if(data.nick == ""){
 			socket.emit("force-login", "Nick can't be empty.");
 			nick = null;
 			return;
 		}
 
-		// If is already in
 		if(users.indexOf(data.nick) != -1){
 			socket.emit("force-login", "This nick is already in chat.");
 			nick = null;
 			return;
 		}
 
-		// Save nick
 		nick = data.nick;
 		users.push(data.nick);
 
 		console.log("User %s joined.", nick.replace(/(<([^>]+)>)/ig, ""));
 		socket.join("main");
 
-		// Tell everyone, that user joined
 		io.to("main").emit("ue", {
 			"nick": nick
 		});
 
-		// Tell this user who is already in
 		socket.emit("start", {
 			"users": users
 		});
 
-		// Send the message cache to the new user
 		console.log(`going to send cache to ${nick}`)
+
 		socket.emit("previous-msg", {
 			"msgs": messageCache
 		});
 	});
 
 	socket.on("send-msg", function(data){
-		// If is logged in
+
 		if(nick == null){
 			socket.emit("force-login", "You need to be logged in to send message.");
 			return;
@@ -79,23 +116,23 @@ io.sockets.on("connection", function(socket){
 
 		const msg = {
 			"f": nick,
-			"m": data.m,
+			"m": filterMessage(data.m),
 			"id": "msg_" + (msg_id++)
 		}
 
 		messageCache.push(msg);
+
 		if(messageCache.length > cache_size){
-			messageCache.shift(); // Remove the oldest message
+			messageCache.shift();
 		}
 
-		// Send everyone message
 		io.to("main").emit("new-msg", msg);
 
 		console.log("User %s sent message.", nick.replace(/(<([^>]+)>)/ig, ""));
 	});
 
 	socket.on("typing", function(typing){
-		// Only logged in users
+
 		if(nick != null){
 			socket.broadcast.to("main").emit("typing", {
 				status: typing,
@@ -107,13 +144,13 @@ io.sockets.on("connection", function(socket){
 	});
 
 	socket.on("disconnect", function(){
+
 		console.log("Got disconnect!");
 
 		if(nick != null){
-			// Remove user from users
+
 			users.splice(users.indexOf(nick), 1);
 
-			// Tell everyone user left
 			io.to("main").emit("ul", {
 				"nick": nick
 			});
